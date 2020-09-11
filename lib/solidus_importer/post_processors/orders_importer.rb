06c73cba546a7b120ae897a50700f2b9bd4ce3f2
Flavio Auciello <flavioauciello@nebulab.it>
2020-09-11 10:17:44 +0200

    [wip] Avoid tax calculation for imported orders

--- lib/solidus_importer/post_processors/orders_importer.rb
+++ lib/solidus_importer/post_processors/orders_importer.rb
@@ -3,10 +3,23 @@
 module SolidusImporter
   module PostProcessors
     class OrdersImporter < SolidusImporter::Processors::Base
+      class CustomAdjuster
+        def initialize(_order)
+        end
+
+        def adjust!
+        end
+      end
+
       def call(ending_context)
         (ending_context[:imported_orders] || []).each do | params|
           begin
+            adjuster = Spree::Config.tax_adjuster_class
+            Spree::Config.tax_adjuster_class = CustomAdjuster
+
             Spree::Core::Importer::Order.import(nil, params)
+
+            Spree::Config.tax_adjuster_class = adjuster
           rescue StandardError => err
             ending_context.merge!(message: err.message)
           end
